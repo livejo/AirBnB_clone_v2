f83d801bcfcfc7f54669843ba75b0a29f344be17
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-08-12 22:17:27 +0300

    updated the commands

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,6 @@
 -- Create a database
--- Create hbnb_test_db if it doesn't exist
+-- Create hbnb_test_db and hbnb_test@localhost if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- Create user:hbnb_test with password 'hbnb_test_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- Privilege to hbnb_test on database: hbnb_test_db
 GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- Privilege to hbnb_test on database: performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
