94639ad9f722da02ec47ade8ef8df9e421f28052
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-08-12 22:15:22 +0300

    removed unnecessary lines

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,6 @@
 -- Create a database
--- Create hbnb_dev_db if it doesn't exist
+-- Create hbnb_dev_db if it doesn't exist and manage the previlages of hbnb_dev@localhost
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user:hbnb_dev with password 'hbnb_dev_pwd'
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Privilege to hbnb_dev for database: hbnb_dev_db
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Privilege to hbnb_dev on database: performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
